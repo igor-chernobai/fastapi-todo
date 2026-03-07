b2bf46f30a6bb997b4642566695dceaa4f7532ed
Igor Chernobai <chernobay.i2112@gmail.com>
2026-03-07 11:45:03 +0200

    refactor: split endpoints into routers

--- app/main.py
+++ app/main.py
@@ -1,39 +1,7 @@
 from fastapi import FastAPI
 
-from app import storage
-from app.schemas import TaskCreate, TaskResponse
-from app.services import create_task, update_task, delete_task
+from app.routers.tasks import task_router
 
 app = FastAPI()
 
-
-@app.get('/tasks', tags=['Tasks'])
-def get_tasks() -> list[TaskResponse]:
-    """
-    Returns a list of all task objects.
-    """
-    return list(storage.tasks.values())
-
-
-@app.post('/tasks', tags=['Tasks'])
-def add_task(task: TaskCreate) -> TaskResponse:
-    """
-    Create a new task and return the created task object.
-    """
-    return create_task(task)
-
-
-@app.put('/tasks/{task_id}', tags=['Tasks'])
-def update_task_view(task_id: int, task_data: TaskCreate):
-    """
-    Update a task by ID and return the updated task.
-    """
-    return update_task(task_id, task_data)
-
-
-@app.delete('/tasks/{task_id}', tags=['Tasks'])
-def delete_task_view(task_id: int):
-    """
-    Delete a task by ID
-    """
-    return delete_task(task_id)
+app.include_router(task_router)
---
+++ app/routers/tasks.py
@@ -0,0 +1,39 @@
+from fastapi import APIRouter
+
+from app import storage
+from app.schemas import TaskCreate, TaskResponse
+from app.services import update_task, delete_task, create_task
+
+task_router = APIRouter(prefix='/tasks', tags=['Tasks'])
+
+
+@task_router.get('/')
+def get_tasks() -> list[TaskResponse]:
+    """
+    Returns a list of all task objects.
+    """
+    return list(storage.tasks.values())
+
+
+@task_router.post('/')
+def create_task_view(task: TaskCreate) -> TaskResponse:
+    """
+    Create a new task and return the created task object.
+    """
+    return create_task(task)
+
+
+@task_router.put('/{task_id}')
+def update_task_view(task_id: int, task_data: TaskCreate):
+    """
+    Update a task by ID and return the updated task.
+    """
+    return update_task(task_id, task_data)
+
+
+@task_router.delete('/{task_id}')
+def delete_task_view(task_id: int):
+    """
+    Delete a task by ID
+    """
+    return delete_task(task_id)
